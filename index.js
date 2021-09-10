322999db323c518bfba2328db053f519edaf93d9
max anselmo <itmanselmo@gmail.com>
2021-09-10 17:14:19 -0400

    output for npm test is broken.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName) {
+    let messages = [];
+    for(let i=0; stringNames.length; i++){
+        messages[i] = `Thank you, ${stringNames[i]}, for the wonderful birthday gift!`;
+    }
+    return messages;
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber != 0) {
+        console.log(positiveNumber);
+    }
+}
